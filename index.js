3ba2473561c0d252fd8948de36294f96f526377c
jmcavaddy <jakemcavaddy@gmail.com>
2023-03-18 15:12:14 -0400

    Added package.json; added inquirer functionality

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response);
+            const markdown = generateMarkdown(response);
+            fs.writeFile('createdREADME.md', markdown, (err) =>
+                err ? console.log(err) : console.log('Success!')
+            );
+        });
+}
 
 // Function call to initialize app
 init();
