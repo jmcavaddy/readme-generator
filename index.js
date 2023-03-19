af30f695981549c4cdd49a1e01c9eabfbe66d734
jmcavaddy <jakemcavaddy@gmail.com>
2023-03-19 01:07:45 -0400

    Added functionality for license badge and info; added code to generate markdown for all responses.

--- index.js
+++ index.js
@@ -9,20 +9,56 @@ const questions = [
         name: 'title',
         message: 'What is the title of your project?'
     },
-
+    {
+        type: 'input',
+        name: 'description',
+        message: "Provide a short description explaining the what, why, and how of your project.\nUse the following questions as a guide: \n - What was your motivation? \n - Why did you build this project? \n - What problem does it solve? \n - What did you learn? \n - What makes your project stand out?"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project.',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Add guidelines for how you would like other developers to contribute to your project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application.\nThen provide examples on how to run them.'
+    },
+    {
+        type: 'input',
+        name: 'questionsGitHubUsername',
+        message: 'Please provide your GitHub username for users to view your other.'
+    },
+    {
+        type: 'input',
+        name: 'questionsEmail',
+        message: 'Please provide an email address with which users can contact you regarding questions about your software.'
+    }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
 function init() {
 
     inquirer.prompt(questions)
         .then((response) => {
             console.log(response);
             const markdown = generateMarkdown(response);
-            fs.writeFile('createdREADME.md', markdown, (err) =>
+            fs.writeFile('generatedREADME.md', markdown, (err) =>
                 err ? console.log(err) : console.log('Success!')
             );
         });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,79 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// A function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  if (data.license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (data.license === 'Apache 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (data.license === 'GPL 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (data.license === 'BSD 3') {
+    return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+  } else {
+    return ''
+  }
+}
 
-// TODO: Create a function that returns the license link
+// A function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) { 
+  if (data.license === 'MIT') {
+    return `https://opensource.org/licenses/MIT`
+  } else if (data.license === 'Apache 2.0') {
+    return `https://opensource.org/licenses/Apache-2.0`
+  } else if (data.license === 'GPL 3.0') {
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  } else if (data.license === 'BSD 3') {
+    return `https://opensource.org/licenses/BSD-3-Clause`
+  } else {
+    return ''
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  if (data.license === 'None') {
+    return ''
+  } else {
+  return `
+## License
+This project is licensed under the ${data.license} license.  
+For more information, please visit [this link](${renderLicenseLink(data)}).
+`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data)}
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+${renderLicenseSection(data)}
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Contact me with questions regarding this software at ${data.questionsEmail}.
+View more of my work at my [GitHub page](https://github.com/${data.questionsGitHubUsername})!
 `;
 }
 
